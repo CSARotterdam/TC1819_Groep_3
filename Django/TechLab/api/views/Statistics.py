cf13b40fc210c1551f212c4d4b154a2176fb84fd
Albert <0961988@hr.nl>
2019-06-05 12:42:26 +0200

    Added different statistics for the API

--- Django/TechLab/api/views/Statistics.py
+++ Django/TechLab/api/views/Statistics.py
@@ -4,7 +4,7 @@ from django.views.generic import View
 from django.shortcuts import get_object_or_404
 import datetime
 
-from api.models import (User)
+from api.models import (User, BorrowItem, Item)
 import json
 
 
@@ -12,6 +12,10 @@ class Statistics(View):
     def get(self, request, *args, **kwargs):
 
         managers_count = User.objects.filter(is_manager=True).count()
+        total_borrowed_items = BorrowItem.objects.all().count()
+        total_late_returned_items = [item for item in BorrowItem.objects.exclude(hand_in_date=None) if item.hand_in_date > (datetime.timedelta(days=item.item.borrow_days) + item.borrow_date)]
+        current_borrowing_items = BorrowItem.objects.filter(hand_in_date=None).count()
+        total_different_items = Item.objects.all().count()
 
         # TODO: all lists of data that should be added.
         '''
@@ -24,5 +28,9 @@ class Statistics(View):
         '''
 
         return JsonResponse(json.loads(json.dumps({
-            'managers': managers_count
-        })), safe=False)
+            'managers': managers_count,
+            'total_borrowed_items': total_borrowed_items,
+            'total_late_returned_items': len(total_late_returned_items),
+            'current_borrow_items': current_borrowing_items,
+            'total_different_items': total_different_items
+        })), safe=False, content_type='application/json')
