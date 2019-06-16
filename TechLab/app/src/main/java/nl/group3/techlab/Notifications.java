3bd2c8c226e9a581f47fef9b29045dbafb10946a
Rahaf <rahafffrahaf97@gmail.com>
2019-06-16 02:23:51 +0200

    Revert "Notification"
    This reverts commit 4a34d828c5a18273dabe692753d95ac2faf61a5c.

--- TechLab/app/src/main/java/nl/group3/techlab/Notifications.java
+++ TechLab/app/src/main/java/nl/group3/techlab/Notifications.java
@@ -1,8 +1,5 @@
 package nl.group3.techlab;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -31,109 +28,97 @@ import java.util.Date;
 import nl.group3.techlab.helpers.JSONHelper;
 
 public class Notifications extends MenuActivity {
-    //Set a notification in 7 days
-    Calendar sevendayalarm = Calendar.getInstance();
-
-//    private EditText editTextTitle;
-//    private EditText editTextDescription;
-//    private Button notify;
-//    Thread thread;
-//    private  NotificationHelper mNotificationHelper;
-//    SharedPreferences sharedPreferences;
-//    ArrayList<JsonObject> borrowedItems;
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private Button notify;
+    Thread thread;
+    private  NotificationHelper mNotificationHelper;
+    SharedPreferences sharedPreferences;
+    ArrayList<JsonObject> borrowedItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        sevendayalarm.add(Calendar.DATE, 7);
+        sharedPreferences = getSharedPreferences("Techlab", 0);
+        int d_color = sharedPreferences.getInt("d_color", 1);
+        switch (d_color) {
+            case 1:
+                setTheme(R.style.theme1);
+                break;
+            case 2:
+                setTheme(R.style.theme2);
+                break;
+            default:
+                break;
+        }
+        int notifications = sharedPreferences.getInt("notifications", 1);
+        switch (notifications) {
+            case 1:
+                settings.notificationOn = true;
+                break;
+            case 2:
+                settings.notificationOn = false;
+                break;
 
-        Intent intent = new Intent(this, Receiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 001, intent, 0);
+            default:
+                break;
+        }
+        borrowedItems = new ArrayList<>();
+        mNotificationHelper = new NotificationHelper(this);
+        if (settings.notificationOn ){
+            SendOnChannel("Return item tomorrow","t2");
+            thread = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        String jsonString = JSONHelper.JSONStringFromURL(String.format( "http://84.86.201.7:8000/api/v1/borrowitems/"), null, 1000, "GET", null);
+                        JsonArray jsonArray = new JsonParser().parse(jsonString).getAsJsonArray();
+                        final GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getBaseContext());
+                        for(JsonElement elem : jsonArray){
+                            JsonObject obj = elem.getAsJsonObject();
+//                        Log.d("?????????", "try again");
+//                        Log.d("ABCDEFG", obj.get("user").getAsJsonObject().get("email").getAsString());
+                            if (acct.getEmail().equals(obj.get("user").getAsJsonObject().get("email").getAsString())) {
+                                borrowedItems.add(obj);
+//                            if (obj.get("item").getAsJsonObject().get("name").getAsString());
+                            }
+                        }
+                    }catch(Exception ex){ ex.printStackTrace();}
+                }
+                
+            });
+            // Start the new thread and run the code.
+            thread.start();
+            // Join the thread when it's done, meaning that the application will wait untill the
+            // thread is done.
+            try {
+                thread.join();
 
-        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, sevendayalarm.getTimeInMillis(), pendingIntent);
-//        super.onCreate(savedInstanceState);
-//        sharedPreferences = getSharedPreferences("Techlab", 0);
-//        int d_color = sharedPreferences.getInt("d_color", 1);
-//        switch (d_color) {
-//            case 1:
-//                setTheme(R.style.theme1);
-//                break;
-//            case 2:
-//                setTheme(R.style.theme2);
-//                break;
-//            default:
-//                break;
-//        }
-//        int notifications = sharedPreferences.getInt("notifications", 1);
-//        switch (notifications) {
-//            case 1:
-//                settings.notificationOn = true;
-//                break;
-//            case 2:
-//                settings.notificationOn = false;
-//                break;
-//
-//            default:
-//                break;
-//        }
-//        borrowedItems = new ArrayList<>();
+            } catch (Exception ex){}
+        }
+        Log.d("Items", borrowedItems + "");
+//        Date currentTime = Calendar.getInstance().getTime();
+//        Time now = new Time();
+//        now.setToNow();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy hh:mmaa");
+        String format = sdf.format(Calendar.getInstance().getTime());
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        Date dateBefore1Day = cal.getTime();
+        Log.d("!!!!!!!!!!!!!", dateBefore1Day+"");
+        setContentView(R.layout.activity_notifications);
+        super.onCreateDrawer();
+        setTitle(R.string.meldingen);
+        notify = (Button)findViewById(R.id.notify);
 //        mNotificationHelper = new NotificationHelper(this);
-//        if (settings.notificationOn ){
-//            SendOnChannel("Return item tomorrow","t2");
-//            thread = new Thread(new Runnable() {
-//                public void run() {
-//                    try {
-//                        String jsonString = JSONHelper.JSONStringFromURL(String.format( "http://84.86.201.7:8000/api/v1/borrowitems/"), null, 1000, "GET", null);
-//                        JsonArray jsonArray = new JsonParser().parse(jsonString).getAsJsonArray();
-//                        final GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getBaseContext());
-//                        for(JsonElement elem : jsonArray){
-//                            JsonObject obj = elem.getAsJsonObject();
-////                        Log.d("?????????", "try again");
-////                        Log.d("ABCDEFG", obj.get("user").getAsJsonObject().get("email").getAsString());
-//                            if (acct.getEmail().equals(obj.get("user").getAsJsonObject().get("email").getAsString())) {
-//                                borrowedItems.add(obj);
-////                            if (obj.get("item").getAsJsonObject().get("name").getAsString());
-//                            }
-//                        }
-//                    }catch(Exception ex){ ex.printStackTrace();}
-//                }
-//
-//            });
-//            // Start the new thread and run the code.
-//            thread.start();
-//            // Join the thread when it's done, meaning that the application will wait untill the
-//            // thread is done.
-//            try {
-//                thread.join();
-//
-//            } catch (Exception ex){}
-//        }
-//        Log.d("Items", borrowedItems + "");
-////        Date currentTime = Calendar.getInstance().getTime();
-////        Time now = new Time();
-////        now.setToNow();
-//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy hh:mmaa");
-//        String format = sdf.format(Calendar.getInstance().getTime());
-//        Calendar cal = Calendar.getInstance();
-//        cal.add(Calendar.DATE, -1);
-//        Date dateBefore1Day = cal.getTime();
-//        Log.d("!!!!!!!!!!!!!", dateBefore1Day+"");
-//        setContentView(R.layout.activity_notifications);
-//        super.onCreateDrawer();
-//        setTitle(R.string.meldingen);
-//        notify = (Button)findViewById(R.id.notify);
-////        mNotificationHelper = new NotificationHelper(this);
-////        notify.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View v) {
-////                SendOnChannel("Return item tomorrow","t2");
-////            }
-////        });
-//    }
-//    public void SendOnChannel(String title, String description) {
-//        NotificationCompat.Builder nb = mNotificationHelper.getCahnnelNotification(title, description);
-//        mNotificationHelper.getMAnager().notify(1,nb.build());
+//        notify.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SendOnChannel("Return item tomorrow","t2");
+//            }
+//        });
+    }
+    public void SendOnChannel(String title, String description) {
+        NotificationCompat.Builder nb = mNotificationHelper.getCahnnelNotification(title, description);
+        mNotificationHelper.getMAnager().notify(1,nb.build());
     }
 
 }
